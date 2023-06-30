afc5322cfb35b9e3b557837d5f2739638037b7b9
Nobel Zhou <nxz157@case.edu>
2023-06-30 17:42:19 -0400

    Removed unnecessary files and comments

--- Experiment Code/stepperRigControl.m
+++ Experiment Code/stepperRigControl.m
@@ -37,13 +37,6 @@ function [data, time] = stepperRigControl(funcV, funcS, pattern, duration, rate)
     if nargin < 5
         rate = 50;
     end
-%     if nargin < 5
-%         setup = 0; % If setup input not provided, default to not doing setup
-% 
-%         % Coder's note: if no input is provided but no setup has been done
-%         % before, this code will still perform setup, irregardless of
-%         % input. - nxz157, 6/16/2023
-%     end
     
     rigUse = [1 1]; % Stores whether the arena and/or stepper are used
     if all(~funcV)
@@ -61,19 +54,6 @@ function [data, time] = stepperRigControl(funcV, funcS, pattern, duration, rate)
     %% Define Constants
     DAQ_RATE = 10000;
     STEPPER_PORT = 'COM3';
-
-%     ARENA_DELAY = 0.004; Removed because stepper is no longer being
-%     triggered by the DAQ
-    
-%     %% Rerun Mappings (if necessary)
-%     if setup || ~isfile('mapping.mat')
-%         mapping = getMapping();
-%     else
-%         fprintf('Skipping re-mapping.\n');
-%         load('mapping.mat', 'mapping');
-%     end
-% 
-%     close all
     
     %% Setup DAQ
     fprintf('Setting up DAQ...');
@@ -84,16 +64,6 @@ function [data, time] = stepperRigControl(funcV, funcS, pattern, duration, rate)
     fprintf('.');
     d.Rate = DAQ_RATE;
     fprintf('.');
-    
-%     % Create DAQ input for arena
-%     daqExpData = zeros(DAQ_RATE * duration, 2);
-    
-%     % Setup output channels
-%     addoutput(d, 'dev1', 'ao0', 'Voltage'); % Output channel for LED Arena (X and Y)
-%     fprintf('.');
-%      
-%     addoutput(d, 'dev1', 'ao1', 'Voltage'); % Output channel for Stepper
-%     fprintf('.');
  
     % Setup input channels
     % Add arena control trigger channel
@@ -162,73 +132,6 @@ function [data, time] = stepperRigControl(funcV, funcS, pattern, duration, rate)
             pause(0.1);
         end
         fprintf('.done\n');
-
-%         fprintf('\tSampling rate...\n');
-%         % Determine indices of change, based on frequency
-%         lastIdx = 1000 / rateV * DAQ_RATE; % Find last index
-%         indices = round(linspace(0, lastIdx, 1001)) + 1; % Find indices of change, starting with 1
-%         indices = indices(1 : 1000); % Disregard last index, as this is when everything finishes and we don't care about the finish point
-%     
-%         % Coder's note: the above is a little confusing. Let's take a 
-%         % somewhat easier example: say we have a 10Hz DAQ, 2Hz pattern 
-%         % velocity, and 3 length sequence. Then, we need to get the 
-%         % duration of the sequence, which is 3 / 2 = 1.5 seconds. In DAQ 
-%         % data points, this is 1.5 * 10 = 15 data points. Note that 15 
-%         % would be the last point, and we need to get the 3 starting 
-%         % points, which would be time 0, time 5, and time 10 using 
-%         % linspace, chopping off the last 15. We increment by 1 to account 
-%         % for dumb MATLAB indexing. - nxz157, 6/15/2023
-%         
-%         funcV = funcX; % Select a function that's not just nothing
-%         if rigUse(1) && ~rigUse(2)
-%             Panel_com('set_mode', [3 4]);
-%             fprintf('\tParsing X function');
-%         elseif ~rigUse(1) && rigUse(2)
-%             Panel_com('set_mode', [4 3]);
-%             fprintf('\tParsing Y function');
-%             funcV = funcY;
-%         else 
-%             Panel_com('set_mode', [3 3]);
-%             fprintf('\tParsing X/Y function');
-%         end
-%         
-%         % Change m-sequence into positional data
-%         arenaMSeq = cumsum(funcV); 
-%         fprintf('.');
-% 
-%         % Coder's note: currently, because of the lack of outputs, X and Y 
-%         % can only be set to the same sequence or none at all. While this 
-%         % code's functionality, with some tweaking, will allow independent 
-%         % X and Y m-sequences, we are currently limited by DAQ space. 
-%         % An alternative is to setup ao1 to be Y and ao0 to be X, but then 
-%         % you would lose stepper functionality. If this is co-opted in the 
-%         % future, this may be helpful. Best of luck, future researcher! 
-%         % - nxz157, 6/15/2023
-%     
-%         % Resolve rollover indices
-%         arenaMSeq(arenaMSeq < 0) = arenaMSeq(arenaMSeq < 0) + 96; 
-%         arenaMSeq(arenaMSeq > 96) = arenaMSeq(arenaMSeq > 96) - 96; 
-%         fprintf('.');
-%         
-%         % Coder's note: the above 2 lines do not account for massive gain. 
-%         % If you have a gain of >10, then you will most likely need to 
-%         % employ a modulo operator to ensure that the cumsum of the 
-%         % m-sequence is within 1 to 96. This should never happen, however, 
-%         % at least for what I'm doing. Change at your (and your flies') 
-%         % own risk. - nxz157, 6/15/2023
-%     
-%         arenaMSeq = arenaMSeq + 1; % Account for MATLAB indexing
-%     
-%         % Loop through each element of X sequence
-%         lastIdx = 1;
-%         for i = 1 : 999
-%             daqExpData(lastIdx : indices(i + 1) - 1, 1) = mapping(arenaMSeq(i)); % Get voltage of index and add its time frame to DAQ
-%             lastIdx = indices(i + 1);
-%         end
-%         fprintf('.');
-% 
-%         daqExpData(indices(1000) : end, 1) = mapping(arenaMSeq(end)); % Append last voltage all the way to the end of the time frame, to prevent a sudden jolt at the end
-%         fprintf('.done\n');
         
         fprintf('\tSetting initial position...\n');
         Panel_com('set_position', [48 48]); % Write first voltage
@@ -278,79 +181,11 @@ function [data, time] = stepperRigControl(funcV, funcS, pattern, duration, rate)
             pause(0.1);
         end
         fprintf('.done\n');
-        
-%         fprintf('\tSampling rate...\n');
-%         
-%         % Determine indices of change, based on frequency
-%         lastIdx = 1000 / rateS * DAQ_RATE; % Find last index
-%         indices = round(linspace(0, lastIdx, 2001)) + 1; % Find indices of change, starting with 1
-%         
-%         % Coder's note: note that this is different than in the arena, as
-%         % the arena linspaces 1000 while the stepper linspaces 2000, even
-%         % though the sequence length is the same. The reason being is that
-%         % because the stepper is not positionally controlled, we are
-%         % sending square waves to the stepper, and thus, I need to reset
-%         % to baseline before sending another step. For instance, if I had
-%         % the sequence of "[1 1]", if I don't reset it between the 2
-%         % indices, then the stepper has no way of knowing if I wanted just
-%         % 1 step or 2 steps. - nxz157, 6/16/2023
-% 
-%         indices = indices(1 : 2000); % Disregard last index, as this is when everything finishes and we don't care about the finish point        
-%         
-%         fprintf('\tSetting gain...\n');
-%         gain = max(abs(funcS)); % Get the max step
-%         Stepper_com(stepper, 'set_sequence_gain', gain);
-%         pause(1);
-% 
-%         % Coder's note: this is just a hack-y way of getting the gain of
-%         % the m-sequence. This should be an integer, with default as 1, but
-%         % sometimes it might be 2 or something else. I could send this as
-%         % an additional input, but to keep everything the same as the
-%         % arena, I opted to encode gain information in the sequence itself.
-%         % - nxz157, 6/16/2023
-%         fprintf('\tParsing stepper function');
-%         stepperMSeq = upsample(funcS, 2); % Add one zero between each sequence (should now be length 2000)
-%         fprintf('.');
-% 
-%         % Get unary m-sequence
-%         stepperMSeq = stepperMSeq / gain * 2.5 + 2.5; % Should now be 0, 2.5, or 5
-%         fprintf('.');
-% 
-%         % Coder's note: because the stepper works by "change" but the arena
-%         % works on position, I need the actual m-sequence for the stepper
-%         % but the cumsum for the arena. - nxz157, 6/16/2023
-% 
-%         % Coder's note: I need to change the m-sequence to a sequence of 0,
-%         % 2.5, and 5, as those are the voltages to indicate left, nothing,
-%         % and right, respectively. - nxz157, 6/16/2023
-%         
-%         daqExpData(1 : ARENA_DELAY * DAQ_RATE) = 2.5;
-%         
-%         % Coder's note: in practice, the arena is about 0.005s slower than
-%         % the arena. To offset this, I am instituting a 0.005s delay on all
-%         % input of the stepper to compensate for this delay. 
-% 
-%         % Loop through each element of X sequence
-%         lastIdx = ARENA_DELAY * DAQ_RATE + 1;
-%         for i = 1 : 1999
-%             daqExpData(lastIdx : indices(i + 1) - 1, 2) = stepperMSeq(i); % Get voltage of index and add its time frame to DAQ
-%             lastIdx = indices(i + 1);
-%         end
-%         fprintf('.');
-%     
-%         daqExpData(indices(2000) : end, 2) = stepperMSeq(end); % Append last voltage all the way to the end of the time frame, to prevent a sudden jolt at the end
-%         fprintf('.done\n');
-
         fprintf('Done setting up Stepper.\n');
     end
     
     %% Final Preparations
     fprintf('Performing final preparations...\n')
-%     fprintf('\tSending DAQ Data..');
-%     preload(d, daqExpData); % Send data to DAQ
-%     pause(2);
-%     fprintf('.done\n');
-    
     fprintf('\tWaiting for user start signal...\n');
     uiwait(msgbox({'Please arm camera and click ok to continue', ...
         ['(Required buffer length ' num2str(duration) ' seconds)']}));
@@ -379,8 +214,13 @@ function [data, time] = stepperRigControl(funcV, funcS, pattern, duration, rate)
     %% Reset and Cleanup Operations
     fprintf('Performing cleanup...\n');
     fprintf('\tStopping Arena...\n');
-    Panel_com('stop'); % Stop
     Panel_com('stop_w_trig'); % Stop triggering and stop arena
+
+    % Coder's note: you may notice that the arena and stepper will start up
+    % again and go through their m-sequence. This is normal and irrelevant,
+    % as data has already been done collecting when this happens. - nxz157,
+    % 6/30/2023
+    
     if rigUse(2)
         fprintf('\tResetting Stepper...\n');
         Stepper_com(stepper, 'reset'); % Reset stepper to exit voltage loop
