ee8b0d1838145a684e0d135578fd66151f53fd42
Nobel Zhou <nxz157@case.edu>
2024-02-27 23:30:10 -0500

    Update functionality to obtain arena voltages in prep for transition to position control mode (AGAIN)

--- Experiment Code/getArenaVoltages.m
+++ Experiment Code/getArenaVoltages.m
@@ -1,7 +1,7 @@
 function voltages = getArenaVoltages
 % getArenaVoltages.m
 % Gets the specific arena voltages for each index.
-% Returns a 10 by 10 matrix representing the voltage at each index for X
+% Returns a 20 row matrix representing the voltage at each index for X
 % and Y.
 %   
 % Author: Nobel Zhou
@@ -12,7 +12,7 @@ function voltages = getArenaVoltages
 % - v0.1 (6/14/2023): Initial commit
 % - v0.2 (6/16/2023): Added comments
 % - v0.3 (2/27/2024): Changed for 10x10 mapping
-
+ 
     fprintf('Getting Arena Voltages...\n');
     %% Setup Panel Controller and DAQ
     % Setup Panel Controller
@@ -21,7 +21,7 @@ function voltages = getArenaVoltages
     Panel_com('set_mode', [3 3]); % Set to position control mode
     pause(0.2);
     fprintf('.');
-    Panel_com('set_pattern_id', 2); % Set to striped pattern
+    Panel_com('set_pattern_id', 1); % Set to striped pattern
     fprintf('.');
     Panel_com('set_position', [1 1]);
     fprintf('.done\n');
@@ -32,28 +32,45 @@ function voltages = getArenaVoltages
     fprintf('.');
     in.Rate = 10000;
     fprintf('.');
-    ch = addinput(in,'dev1','ai4','Voltage'); % AI4 is Arena Y input
+    ch = addinput(in,'dev1','ai10','Voltage'); % AI10 is Arena X input
+    fprintf('.');
+    ch = addinput(in,'dev1','ai3','Voltage'); % AI3 is Arena X input
     fprintf('.');
     ch.TerminalConfig = 'SingleEnded';
     fprintf('.done\n');
     
     % Setup Output
     fprintf('\tCommencing voltage operations...\n');
-    voltages = zeros(1, 96);
+    voltages = zeros(1, 20);
     
     %% Loop Through Indices
-    for i = 1 : 96
-        fprintf(['\t\tDetermining Voltage of Frame ' num2str(i) ' of 96']);
-        Panel_com('set_position', [1 i]); % Set position to index
+    for i = 1 : 10
+        fprintf(['\t\tX: Determining Voltage of Frame ' num2str(i) ' of 10']);
+        Panel_com('set_position', [i 1]); % Set position to index
         fprintf('.');
         pause(2);
         fprintf('.');
     
         TT = read(in, seconds(0.5)); % Block MATLAB and collect voltage data for 0.5 seconds
         fprintf('.');
-        data = TT.('Dev1_ai4');
+        data = TT.Dev1_ai10; % Read Arena X
         fprintf('.');
         voltages(i) = mean(data);
+        fprintf('.done\n');
+    end
+
+    for j = 1 : 10
+        fprintf(['\t\tY: Determining Voltage of Frame ' num2str(j) ' of 10']);
+        Panel_com('set_position', [1 j]); % Set position to index
+        fprintf('.');
+        pause(2);
+        fprintf('.');
+    
+        TT = read(in, seconds(0.5)); % Block MATLAB and collect voltage data for 0.5 seconds
+        fprintf('.');
+        data = TT.Dev1_ai3; % Read Arena Y
+        fprintf('.');
+        voltages(j + 10) = mean(data);
         fprintf('.done\n');
     end
 end
--- Experiment Code/getMapping.m
+++ Experiment Code/getMapping.m
@@ -1,7 +1,7 @@
-  function mapping = getMapping
+  function mappingX = getMapping
 % getMapping.m
 % Gets the specific arena voltages for each index.
-% Returns a 96-length row vector representing the voltage at each index
+% Returns a 100-length row vector representing the voltage at each index
 %   
 % Author: Nobel Zhou
 % Date: 14 June 2023
@@ -12,6 +12,7 @@
 
     fprintf('Getting Mapping...\n');
     %% Get Arena Voltages
+    load('voltages.mat', 'voltages');
     voltages = getArenaVoltages();
     save('voltages', 'voltages');
     
@@ -24,10 +25,10 @@
     Panel_com('set_mode', [3 3]); % Set to position control mode
     pause(2);
     fprintf('.');
-    Panel_com('set_pattern_id', 2); % Set to striped pattern
+    Panel_com('set_pattern_id', 1); % Set to striped pattern
     pause(2);
     fprintf('.');
-    Panel_com('send_gain_bias', [10 0 10 0]);
+    Panel_com('send_gain_bias', [100 0 100 0]);
     pause(1);
     fprintf('.');
     Panel_com('start'); % Start panel controller using voltage setting
@@ -40,7 +41,8 @@
     fprintf('.');
     in.Rate = 10000;
     fprintf('.');
-    ch = addinput(in,'dev1','ai4','Voltage'); % AI4 is Arena Y input
+    ch = addinput(in,'dev1','ai10','Voltage'); % AI10 is Arena X input
+    ch = addinput(in,'dev1','ai3','Voltage'); % AI3 is Arena Y input
     fprintf('.');
     ch.TerminalConfig = 'SingleEnded';
     fprintf('.');
@@ -49,35 +51,39 @@
     fprintf('.');
     out.Rate = 10000;
     fprintf('.');
-    addoutput(out,'dev1','ao0','Voltage');
+    addoutput(out,'dev1','ao0','Voltage'); % Arena X output
+    addoutput(out,'dev1','ao1','Voltage'); % Arena Y output
     fprintf('.');
     fprintf('.done\n');
     
     fprintf('\tCommencing mapping operation...\n');
     % Use previous mapping file if supplied
     if isfile('mapping.mat')
-        load('mapping.mat', 'mapping');
+        load('mapping.mat', 'mappingX', 'mappingY');
     else
-        mapping = linspace(0, 950 / 1023 * 5, 96); % Generate good "guesses" for voltages
+        % Generate good "guesses" for voltages
+        mappingX = linspace(0, 1000 / 1023 * 5, 10); 
+        mappingY = linspace(0, 1000 / 1023 * 5, 10);
     end
-    for i = 1 : 96
+
+    for j = 1 : 10
         voltageCompliant = 0;
-        fprintf(['\t\tMapping Frame ' num2str(i) ' of 96...\n']);
+        fprintf(['\t\tY: Mapping Frame ' num2str(j) ' of 10...\n']);
         while ~voltageCompliant
-            v = mapping(i);
+            v = mappingY(j);
             fprintf(['\t\t\tTrying voltage of ' num2str(v) '...\n']);
-            write(out, v);
+            write(out, [0, v]);
     
             TT = read(in, seconds(5)); % Block MATLAB and collect voltage data for 5 seconds
-            data = TT.('Dev1_ai4');
+            data = TT.Dev1_ai3;
             plot(data);
             drawnow;
-            data = data - voltages(i);
-            if (~isempty(find(data > 0.035, 1)))
-                mapping(i:end) = mapping(i:end) - 0.002;
+            data = data - voltages(j + 10);
+            if (~isempty(find(data > 0.1, 1)))
+                mappingY(j:end) = mappingY(j:end) - 0.1;
                 fprintf('\t\t\t\tMapping noncompliant. Decrementing voltage...\n');
-            elseif (~isempty(find(data < -0.035, 1)))
-                mapping(i:end) = mapping(i:end) + 0.002;
+            elseif (~isempty(find(data < -0.1, 1)))
+                mappingY(j:end) = mappingY(j:end) + 0.1;
                 fprintf('\t\t\t\tMapping noncompliant. Incrementing voltage...\n');
             else
                 fprintf('\t\t\tMapping compliant. Proceeding to next index...\n');
@@ -85,8 +91,35 @@
             end
         end
     end
-    fprintf('\tSaving mapping...\n');
-    save('mapping', 'mapping');
-    fprintf('Mapping done.\n');
 
+    for i = 1 : 10
+        voltageCompliant = 0;
+        fprintf(['\t\tX: Mapping Frame ' num2str(i) ' of 10...\n']);
+        while ~voltageCompliant
+            v = mappingX(i);
+            fprintf(['\t\t\tTrying voltage of ' num2str(v) '...\n']);
+            write(out, [v, 0]);
+    
+            TT = read(in, seconds(5)); % Block MATLAB and collect voltage data for 5 seconds
+            data = TT.Dev1_ai10;
+            plot(data);
+            drawnow;
+            data = data - voltages(i);
+            if (~isempty(find(data > 0.1, 1)))
+                mappingX(i:end) = mappingX(i:end) - 0.1;
+                fprintf('\t\t\t\tMapping noncompliant. Decrementing voltage...\n');
+            elseif (~isempty(find(data < -0.1, 1)))
+                mappingX(i:end) = mappingX(i:end) + 0.1;
+                fprintf('\t\t\t\tMapping noncompliant. Incrementing voltage...\n');
+            else
+                fprintf('\t\t\tMapping compliant. Proceeding to next index...\n');
+                voltageCompliant = 1;
+            end
+        end
+    end
+
+    fprintf('\tSaving mapping...\n');
+    save('mapping', 'mappingX', 'mappingY');
+    fprintf('Mapping done.\n');
+    
 end
